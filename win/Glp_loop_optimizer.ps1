a2050c85bda1562fa9b91debc2d76e12ed0f3d8e
CRTYPUBG <a95585995@gmail.com>
2025-06-19 17:11:50 +0300

    Update Glp_loop_optimizer.ps1

--- win/Glp_loop_optimizer.ps1
+++ win/Glp_loop_optimizer.ps1
@@ -1,48 +1,75 @@
 Add-Type -AssemblyName PresentationFramework
 
-[xml]$xaml = @"
-<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
-        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
-        Title="GameLoop Optimizer" Height="180" Width="320"
-        WindowStartupLocation="CenterScreen"
-        ResizeMode="NoResize">
-    <Grid Margin="10">
-        <Grid.RowDefinitions>
-            <RowDefinition Height="Auto"/>
-            <RowDefinition Height="50"/>
-            <RowDefinition Height="*"/>
-        </Grid.RowDefinitions>
-        <TextBlock x:Name="TitleText" Text="GameLoop Optimizer" FontSize="18" FontWeight="Bold" HorizontalAlignment="Center" Margin="0,0,0,10"/>
-        <Button x:Name="StartButton" Grid.Row="1" Content="Optimize Başlat" Width="150" Height="35" HorizontalAlignment="Center" />
-        <TextBlock x:Name="StatusText" Grid.Row="2" Text="Durum: Bekleniyor..." FontSize="14" HorizontalAlignment="Center" VerticalAlignment="Center" TextWrapping="Wrap" />
-    </Grid>
-</Window>
-"@
+# Pencere oluştur
+$Window = New-Object System.Windows.Window
+$Window.Title = "GameLoop Optimizer"
+$Window.Width = 320
+$Window.Height = 180
+$Window.WindowStartupLocation = "CenterScreen"
+$Window.ResizeMode = "NoResize"
 
-# Load XAML
-$reader = (New-Object System.Xml.XmlNodeReader $xaml)
-$Window = [Windows.Markup.XamlReader]::Load($reader)
+# Grid oluştur
+$Grid = New-Object System.Windows.Controls.Grid
+$Grid.Margin = [System.Windows.Thickness]::new(10)
 
-# Find named elements
-$StartButton = $Window.FindName('StartButton')
-$StatusText = $Window.FindName('StatusText')
+# Satır tanımları
+$rowDef1 = New-Object System.Windows.Controls.RowDefinition
+$rowDef1.Height = [System.Windows.GridLength]::new([System.Windows.GridUnitType]::Auto)
+$rowDef2 = New-Object System.Windows.Controls.RowDefinition
+$rowDef2.Height = [System.Windows.GridLength]::new(50)
+$rowDef3 = New-Object System.Windows.Controls.RowDefinition
+$rowDef3.Height = [System.Windows.GridLength]::new([System.Windows.GridUnitType]::Star)
 
-# Optimize fonksiyon (örnek)
+$Grid.RowDefinitions.Add($rowDef1)
+$Grid.RowDefinitions.Add($rowDef2)
+$Grid.RowDefinitions.Add($rowDef3)
+
+# Başlık TextBlock
+$TitleText = New-Object System.Windows.Controls.TextBlock
+$TitleText.Text = "GameLoop Optimizer"
+$TitleText.FontSize = 18
+$TitleText.FontWeight = 'Bold'
+$TitleText.HorizontalAlignment = 'Center'
+$TitleText.Margin = [System.Windows.Thickness]::new(0,0,0,10)
+[System.Windows.Controls.Grid]::SetRow($TitleText, 0)
+$Grid.Children.Add($TitleText) | Out-Null
+
+# Başlat Butonu
+$StartButton = New-Object System.Windows.Controls.Button
+$StartButton.Content = "Optimize Başlat"
+$StartButton.Width = 150
+$StartButton.Height = 35
+$StartButton.HorizontalAlignment = 'Center'
+[System.Windows.Controls.Grid]::SetRow($StartButton, 1)
+$Grid.Children.Add($StartButton) | Out-Null
+
+# Durum TextBlock
+$StatusText = New-Object System.Windows.Controls.TextBlock
+$StatusText.Text = "Durum: Bekleniyor..."
+$StatusText.FontSize = 14
+$StatusText.HorizontalAlignment = 'Center'
+$StatusText.VerticalAlignment = 'Center'
+$StatusText.TextWrapping = 'Wrap'
+[System.Windows.Controls.Grid]::SetRow($StatusText, 2)
+$Grid.Children.Add($StatusText) | Out-Null
+
+# Grid'i pencereye ekle
+$Window.Content = $Grid
+
+# Optimize fonksiyonu
 function Optimize-GameLoop {
     $StatusText.Text = "Optimizasyon başladı..."
     Start-Sleep -Seconds 1
 
-    # Buraya gerçek optimizasyon komutlarını ekle
-    # Örneğin;
-    # Set-ProcessPriority, registry ayarları, gamebar kapatma vb.
+    # Buraya optimizasyon kodlarını ekle
 
     $StatusText.Text = "Optimizasyon tamamlandı! FPS artışı deneyin."
 }
 
-# Buton click eventi
+# Buton click eventi ekle
 $StartButton.Add_Click({
     Optimize-GameLoop
 })
 
-# Show Window
+# Pencereyi göster
 $Window.ShowDialog() | Out-Null
